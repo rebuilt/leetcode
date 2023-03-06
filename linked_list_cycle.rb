e9de02caba9708c6e653054f237efbc21d4c96dd
Nelson Jovel <memoryman51@hotmail.com>
2023-03-05 20:47:22 -0800

    checks if there is a cycle in the list but blows the stack if the list is too large

--- linked_list_cycle.rb
+++ linked_list_cycle.rb
@@ -7,24 +7,18 @@ class ListNode
 end
 
 def hasCycle(head)
-  return false if head.next == nil
-  return true if node_hash[head&.next] != nil
-  node_hash[head&.next] = head&.next
-  puts head&.next
-  puts head.next
+  return false if head == nil || head.next == nil 
+  return true if head.next == head.next.next&.next
   hasCycle(head.next)
 end
 
-def node_hash
- @node_hash ||= {}
-end
 
 
 node5 = ListNode.new(val: 5, next_node: nil)
 node4 = ListNode.new(val: 4, next_node: node5)
 node3 = ListNode.new(val: 3, next_node: nil)
 node2 = ListNode.new(val: 2, next_node: node3)
-node3.next = node2
+# node3.next = node2
 node1 = ListNode.new(val: 1, next_node: node2)
 
 puts hasCycle(node1)
