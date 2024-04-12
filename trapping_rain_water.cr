0f1e7bc7a09e4c3fc040ed993f53ca396fbb079e
Nelson Jovel <memoryman51@hotmail.com>
2024-04-12 12:40:42 -0700

    solution to trapping water

--- trapping_rain_water.cr
+++ trapping_rain_water.cr
@@ -1,18 +1,21 @@
 def trap(heights)
   # Compute in the forward direction
-  water_amount_1, last_index = compute(heights)
+  result = compute(heights)
+  water_amount_1 : Int32? = result[0] || 0
+  last_index = result[1] || 0
   # Chop off all values that we've already been able to compute and compute again in the reverse direction
-  water_amount_2, last_index = compute(heights[last_index, heights.length - 1].reverse)
+  water_amount_2 : Int32? = compute(heights[last_index.to_i, heights.size() - 1].reverse)[0] || 0
 
   water_amount_1 + water_amount_2
 end
 
-def compute(heights)
+def compute(heights) 
   left = nil
   right_index = nil
-  output = []
-  tmp_water = []
+  output =  [] of Int32
+  tmp_water = [] of Int32
 
+  # iterate in the forward direction
   heights.each_with_index do |height, index|
     # Look for left boundary if it's nil
     if left.nil? && height.positive?
@@ -27,10 +30,12 @@ def compute(heights)
     if left > 0 && height >= left
       # found the right boundary
       # add the temporary water to the result
-      output << tmp_water
+      tmp_water.each do  |item|
+        output << item
+      end
       left = height
       right_index = index
-      tmp_water = []
+      tmp_water = Array(Int32).new
       next
     end
     # Anything else might contain water we need to add to the result
@@ -38,10 +43,10 @@ def compute(heights)
     tmp_water << left - height
   end
 
-  # Add up all the values in the output array
   amount_of_water = output.flatten.compact.reduce(0) { |num, acc| num + acc }
   [amount_of_water, right_index]
 end
+
 
 heights = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 puts trap(heights)
